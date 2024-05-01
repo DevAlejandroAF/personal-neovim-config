1105c665770d7f4146bc1e646a0fcf1e88b5efe5
DevAlejandroAF <alejandroavi95@hotmail.com>
2024-04-30 22:21:03 -0500

    Initial commit, config 04/2024

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "Shatur/neovim-ayu",
+    lazy = true,
+    name = "ayu",
+    opts = {
+      theme = "ayu-dark",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    opts = {
+      theme = "wave",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
