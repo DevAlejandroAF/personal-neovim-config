bb2a86f80e19619bf2917b273f85ee0dbbaa89a3
DevAlejandroAF <alejandroavi95@hotmail.com>
2024-06-19 19:59:19 -0500

    some little configurations more

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,11 +1,14 @@
+-- Options
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.g.background = "light"
-
 vim.opt.swapfile = false
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.wo.relativenumber = true
+vim.opt.cursorline = true
 
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
@@ -13,11 +16,10 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-vim.wo.number = true
 -- Personal keymaps
 vim.keymap.set('i', 'jk', '<Esc>', { noremap = true })
 vim.keymap.set('n', '<leader>bd', ':Neotree toggle<CR> <bar> :bd<CR> <bar> :bn<CR> <bar> :Neotree toggle<CR>')
 vim.keymap.set('n', '<leader>l', ':Lazy<CR>')
 vim.keymap.set('n', '<leader>m', ':Mason<CR>')
 vim.keymap.set('n', '<leader>qq', ':qa<CR>')
+vim.keymap.set('n', '<leader>w', ':set wrap!<CR>')
