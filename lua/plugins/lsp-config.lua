a42a2eda5dab53d81984ab68b0f9bfa403011397
DevAlejandroAF <alejandroavi95@hotmail.com>
2024-06-20 18:14:28 -0500

    lsp configuration done

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,16 +20,38 @@ return {
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require("lspconfig")
+
       lspconfig.tsserver.setup({
         capabilities = capabilities
       })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
       lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.astro.setup({
+        capabilities = capabilities
+      })
+      lspconfig.emmet_language_server.setup({
+        capabilities = capabilities
+      })
+      lspconfig.gopls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.jsonls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.prettier.setup({
+        capabilities = capabilities
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities
+      })
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities
+      })
+      lspconfig.volar.setup({
+        capabilities = capabilities
+      })
+      lspconfig.eslint.setup({
         capabilities = capabilities
       })
 
