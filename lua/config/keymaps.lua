6eed18fb8fbda022642fef8047d65e5986f07111
DevAlejandroAF <alejandroavi95@hotmail.com>
2024-07-31 15:54:51 -0500

    TmuxNavigation keymaps modified

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,8 +4,21 @@
 --
 local map = vim.keymap.set
 
+-- Remap ESC
 map("i", "jk", "<ESC>", { desc = "Exit insert mode" })
+
+-- BufferLine
 map("n", "<leader>[", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev Buffer" })
 map("n", "<leader>]", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer" })
+
+-- Oil
 map("n", "<leader>-", "<cmd>Oil<cr>", { desc = "Oil" })
+
+-- Mason
 map("n", "<leader>m", "<cmd>Mason<cr>", { desc = "Mason" })
+
+-- TmuxNavigation
+map("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", {})
+map("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", {})
+map("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", {})
+map("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", {})
