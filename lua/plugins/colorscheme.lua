a8c4173a80b438b0531d3f21b3684eb92567b009
DevAlejandroAF <alejandroavi95@hotmail.com>
2024-08-28 21:01:07 -0500

    lsp & formatting config added

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -26,7 +26,21 @@ return {
         overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
         palette_overrides = {},
       })
-      vim.cmd.colorscheme("vesper")
+      -- vim.cmd.colorscheme("vesper")
+    end,
+  },
+  {
+    "shatur/neovim-ayu",
+    lazy = false,
+    name = "ayu",
+    priority = 1000,
+    config = function()
+      require("ayu").setup({
+        mirage = false, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
+        terminal = true, -- Set to `false` to let terminal manage its own colors.
+        overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+      })
+      vim.cmd.colorscheme("ayu")
     end,
   },
 }
