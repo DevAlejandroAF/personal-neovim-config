2a4903b38969bf566eb2ccf1086a528c5b72ef88
Alejandro Ávila <alejandroavi95@hotmail.com>
2025-08-13 16:05:38 -0500

    Poimandres theme highlight trouble resolved

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -18,41 +18,41 @@ return {
   --       vim.cmd.colorscheme("bluloco")
   --     end,
   --   },
-  --  {
-  --    "datsfilipe/vesper.nvim",
-  --    lazy = false,
-  --    name = "vesper",
-  --    priority = 1000,
-  --    config = function()
-  --      require("vesper").setup({
-  --        transparent = false, -- Boolean: Sets the background to transparent
-  --        italics = {
-  --          comments = true, -- Boolean: Italicizes comments
-  --          keywords = true, -- Boolean: Italicizes keywords
-  --          functions = true, -- Boolean: Italicizes functions
-  --          strings = true, -- Boolean: Italicizes strings
-  --          variables = true, -- Boolean: Italicizes variables
-  --        },
-  --        overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
-  --        palette_overrides = {},
-  --      })
-  --      vim.cmd.colorscheme("vesper")
-  --    end,
-  --  },
-  {
-    "shatur/neovim-ayu",
-    lazy = false,
-    name = "ayu",
-    priority = 1000,
-    config = function()
-      require("ayu").setup({
-        mirage = false, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
-        terminal = true, -- Set to `false` to let terminal manage its own colors.
-        overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
-      })
-      vim.cmd.colorscheme("ayu")
-    end,
-  },
+  -- {
+  --   "datsfilipe/vesper.nvim",
+  --   lazy = false,
+  --   name = "vesper",
+  --   priority = 1000,
+  --   config = function()
+  --     require("vesper").setup({
+  --       transparent = false, -- Boolean: Sets the background to transparent
+  --       italics = {
+  --         comments = true, -- Boolean: Italicizes comments
+  --         keywords = true, -- Boolean: Italicizes keywords
+  --         functions = true, -- Boolean: Italicizes functions
+  --         strings = true, -- Boolean: Italicizes strings
+  --         variables = true, -- Boolean: Italicizes variables
+  --       },
+  --       overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
+  --       palette_overrides = {},
+  --     })
+  --     vim.cmd.colorscheme("vesper")
+  --   end,
+  -- },
+  -- {
+  --   "shatur/neovim-ayu",
+  --   lazy = false,
+  --   name = "ayu",
+  --   priority = 1000,
+  --   config = function()
+  --     require("ayu").setup({
+  --       mirage = false, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
+  --       terminal = true, -- Set to `false` to let terminal manage its own colors.
+  --       overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+  --     })
+  --     vim.cmd.colorscheme("ayu")
+  --   end,
+  -- },
   -- {
   --   "rose-pine/neovim",
   --   name = "rose-pine",
@@ -76,11 +76,19 @@ return {
   --      vim.cmd.colorscheme("kanagawa-wave")
   --    end,
   --  },
-  -- {
-  --   "olivercederborg/poimandres.nvim",
-  --   name = "poimandres",
-  --   config = function()
-  --     vim.cmd.colorscheme("poimandres")
-  --   end,
-  -- },
+  {
+    "olivercederborg/poimandres.nvim",
+    name = "poimandres",
+    config = function()
+      local p = require("poimandres.palette")
+      require("poimandres").setup({
+        highlight_groups = {
+          LspReferenceText = { bg = p.background1 },
+          LspReferenceRead = { bg = p.background1 },
+          LspReferenceWrite = { bg = p.background1 },
+        },
+      })
+      vim.cmd.colorscheme("poimandres")
+    end,
+  },
 }
